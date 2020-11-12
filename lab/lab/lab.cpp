13c1fcd1012a18469804438bd4391a68973e6a36
Anh Nguyen <S3864077@rmit.edu.vn>
2020-11-12 11:36:16 +0700

    finished

--- lab/lab/lab.cpp
+++ lab/lab/lab.cpp
@@ -1,20 +1,52 @@
-// lab.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <fstream>
 
-int main()
+using namespace std;
+
+bool is_float(const char *val)
 {
-    std::cout << "Hello World!\n";
+	int i = 0;
+	int dot_counter = 0;
+	if (val[0] == '+' || val[0] == '0') i++;
+	while (val[i] != '\0'){
+		if (val[i] == '.'){
+			dot_counter++;
+			if (dot_counter > 1)
+			return false;
+		}
+		else if (val[i] < '0' || val[i] > '9')
+			return false;
+		i++;
+	}
+	return true;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main(int argc, char* argv[])
+{
+	// cerr can be redirected 
+	streambuf* cerrbuf = cerr.rdbuf();	// Pointer which points to the output error stream
+	ofstream errorout("error.txt");		// Declaring and initializing an output filestream
+	cerr.rdbuf(errorout.rdbuf());		// Redirecting cerr to the output filestream created above
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	for (int i = 1; i < 10; i++)
+	{
+		string input;
+		cout << "enter float number:" << endl;
+		cin >> input;
+		//if (!isInteger_pointer(argv[i]))
+		//if (!isInteger_loop(argv[i]))
+		if (!is_float(input.c_str()))
+		{
+			cerr<< input << " is a invalid integer format" << endl;
+		}
+		else
+		{
+			cout << input << " is a valid float format" << endl;
+		}
+	}
+	cerr.rdbuf(cerrbuf);	// Redirecting cerr back to the console
+	cerr << "This is an output error stream which should be seen at the console" << endl;
+
+	return 0;
+}
